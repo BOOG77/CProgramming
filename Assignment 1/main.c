537c1b103c3f7fa71c62822fbba9c31ec2c4fcaf
BOOG77 <dawsonb14@hotmail.com>
2025-02-22 23:38:27 -0400

    assignment 1 complete no comments

--- Assignment 1/main.c
+++ Assignment 1/main.c
@@ -4,59 +4,59 @@ int main(){
     char name[100];
     int enterStudent;
     int studentCount = 0;
-    int gradeTotal;
-    int gradeAvg;
+    float gradeTotal = 0;
+    float gradeAvg;
+    float grade1, grade2, grade3;
 
     // prompt the user to input student details (name, grades for 3 subjects).
 
     do{
+        float studentTotal = 0;
         // enter name
         printf("Please enter the student name:");
         scanf(" %[^\n]", &name);
         printf("You entered the name as: %s \n", name);
-        
-        // grades for 3 subjects initialized
-        int grade1, grade2, grade3;
 
         // 1. Prompt the user
-        printf("Enter grade for Subject 1: ");
-        scanf("%d", &grade1);
+        printf("Enter grade for Subject 1:");
+        scanf("%f", &grade1);
 
         // 2. Validate the input
         if(grade1 < 0 || grade1 > 100){
-            printf("Enter grade for Subject 1(0-100): ");
-            scanf("%d", grade1);
+            printf("Enter grade for Subject 1(0-100):");
+            scanf("%f", &grade1);
         }
 
-        printf("Enter grade for Subject 2: ");
-        scanf("%d", &grade2);
+        printf("Enter grade for Subject 2:");
+        scanf("%f", &grade2);
         if(grade2 < 0 || grade2 > 100){
-            printf("Enter grade for Subject 2(0-100): ");
-            scanf("%d", grade2);
+            printf("Enter grade for Subject 2(0-100):");
+            scanf("%f", &grade2);
         }
 
-        printf("Enter grade for Subject 3: ");
-        scanf("%d", &grade3);
+        printf("Enter grade for Subject 3:");
+        scanf("%f", &grade3);
         if(grade1 < 0 || grade1 > 100){
-            printf("Enter grade for Subject 3(0-100): ");
-            scanf("%d", grade3);
+            printf("Enter grade for Subject 3(0-100):");
+            scanf("%f", &grade3);
         }
 
         // Calculate and display
             // total marks
-            gradeTotal = grade1 + grade2 + grade3;
-            printf("Total Marks: %d", gradeTotal);
+            studentTotal = grade1 + grade2 + grade3;
+            gradeTotal = studentTotal + gradeTotal;
+            printf("Total Marks: %f \n", studentTotal);
 
             // average marks
-            gradeAvg = gradeTotal / 3;
-            printf("Average Marks: %d", gradeAvg);
+            gradeAvg = studentTotal / 3;
+            printf("Average Marks: %f \n", gradeAvg);
 
             // pass status
             if(gradeAvg >= 50){
-                printf("Status: Passed");
+                printf("Status: Passed \n");
             }
             else{
-                printf("Status: Failed");
+                printf("Status: Failed \n");
             }
 
         studentCount++;
@@ -68,5 +68,5 @@ int main(){
 
     printf("Data summary\n");
     printf("Number of students: %d\n", studentCount);
-    printf("Total marks: %d", gradeTotal);
+    printf("Class Average: %f", gradeTotal / (studentCount * 3));
 }
